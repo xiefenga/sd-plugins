e990a14a0dec38efd573516ac0f596d5e037d6b0
xiefenga <1294699027@qq.com>
2023-03-11 18:32:08 +0800

    feat: 修改打包名

--- packages/sd-plugin-scripts/src/node/scripts/pack.ts
+++ packages/sd-plugin-scripts/src/node/scripts/pack.ts
@@ -1,9 +1,13 @@
 import glob from 'glob'
+import dayjs from 'dayjs'
 import fse from 'fs-extra'
 import AdmZip from 'adm-zip'
 import path from 'node:path'
+import { formatBytes } from '../utils/fmt'
 import { PLUGIN_CONFIG_JSON } from '../utils/files'
 import { DIST_PATH, PLUGIN_CONFIG_TEMP_PATH, PLUGIN_PATH, PLUGIN_TEMP_PATH } from '../config/paths'
+
+const moment = dayjs()
 
 // 清空目录
 fse.removeSync(PLUGIN_TEMP_PATH)
@@ -26,13 +30,39 @@ const zip = new AdmZip()
 
 zip.addLocalFolder(PLUGIN_TEMP_PATH)
 
-const pluginName = `plugin-${new Date().getTime()}.zip`
+const pluginName = configJson.name ?? ''
 
-const pluginPath = path.resolve(PLUGIN_PATH, pluginName)
+const filename = `${pluginName}-${moment.format('YYYY-MM-DD')}.zip`
+
+const pluginPath = path.resolve(PLUGIN_PATH, filename)
+
+const bakPath = {
+  prev: pluginPath,
+  next: pluginPath,
+}
+
+let i = 1
+
+while (fse.existsSync(bakPath.next)) {
+  bakPath.prev = bakPath.next
+  bakPath.next = `${pluginPath}.${i++}`
+}
+
+if (i > 1) {
+  fse.renameSync(bakPath.prev, bakPath.next)
+}
 
 zip.writeZip(pluginPath)
 
-console.log(pluginName)
+const pluginSize = fse.statSync(pluginPath).size
+
+console.log('插件构建完成🎉🎉🎉')
+
+console.log(`
+插件名: ${filename}
+构建时间: ${moment.format('YYYY-MM-DD HH:mm:ss')}
+大小: ${formatBytes(pluginSize)}
+`)
 
 
 
